6579c1ec5f76cee1c91183c2a5a2921154f327a2
toueileb <toueilebmhamed@gmail.com>
2023-10-14 11:43:41 +0200

    First commt

---
+++ src/main/java/org/sid/comptemanagement/CompteManagementApplication.java
@@ -0,0 +1,13 @@
+package org.sid.comptemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteManagementApplication.class, args);
+    }
+
+}
