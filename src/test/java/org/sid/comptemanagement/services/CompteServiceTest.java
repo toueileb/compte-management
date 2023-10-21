0ab175606504eba4aea8d613b692f4885f7bf5de
Mohamed TOUEILEB <mtoueileb.ext@softwaymedical.fr>
2023-10-21 15:16:24 +0200

    :test_tube: Add modify unit test

--- src/test/java/org/sid/comptemanagement/services/CompteServiceTest.java
+++ src/test/java/org/sid/comptemanagement/services/CompteServiceTest.java
@@ -6,7 +6,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.sid.comptemanagement.dto.CompteDto;
 import org.sid.comptemanagement.dto.CompteRequestDto;
 import org.sid.comptemanagement.entities.Compte;
 import org.sid.comptemanagement.repositories.CompteRepository;
@@ -14,7 +13,8 @@ import org.sid.comptemanagement.repositories.CompteRepository;
 import java.math.BigDecimal;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
